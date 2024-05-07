fbf957f3a32cadac462d02a08839b14b9c2e14a3
Hleb Nikalayenka <s28311@pjwstk.edu.pl>
2024-05-07 11:00:13 +0200

    Change returned values from task information

--- APBDTest1/ProjectController.cs
+++ APBDTest1/ProjectController.cs
@@ -20,7 +20,7 @@ public class ProjectController
 
     public async Task<TaskInfo?> GetTask(int idTask)
     {
-        await using SqlCommand command = new SqlCommand("SELECT IdTask, Task.Name, Description, Deadline, IdProject, TaskType.Name, IdAssignedTo, IdCreator FROM Task JOIN TaskType ON Task.IdTaskType = TaskType.IdTaskType WHERE IdTask = @IdTask", this._connection);
+        await using SqlCommand command = new SqlCommand("SELECT Task.Name, Description, Task.Deadline, Project.Name, TaskType.Name FROM Task JOIN TaskType ON Task.IdTaskType = TaskType.IdTaskType JOIN Project ON Task.IdProject = Project.IdProject WHERE IdTask = @IdTask", this._connection);
         command.Parameters.AddWithValue("@IdTask", idTask);
         await using SqlDataReader reader = await command.ExecuteReaderAsync();
         if (!await reader.ReadAsync())
@@ -28,24 +28,22 @@ public class ProjectController
             return null;
         }
 
-        return new TaskInfo((int)reader[0], (string)reader[1], (string)reader[2], (DateTime)reader[3], (int)reader[4],
-            (string)reader[5], (int)reader[6],
-            (int)reader[7]);
+        return new TaskInfo((string)reader[0], (string)reader[1], (DateTime)reader[2], (string)reader[3],
+            (string)reader[4]);
     }
 
     public async Task<TeamMemberInfo?> GetMemberInfo(int idTeamMember)
     {
         List<TaskInfo> tasks = new List<TaskInfo>();
-        await using (SqlCommand command = new SqlCommand("SELECT IdTask, Task.Name, Description, Deadline, IdProject, TaskType.Name, IdAssignedTo, IdCreator FROM Task JOIN TaskType ON Task.IdTaskType = TaskType.IdTaskType WHERE IdAssignedTo = @IdAssignedTo ORDER BY DeadLine DESC", this._connection))
+        await using (SqlCommand command = new SqlCommand("SELECT Task.Name, Description, Task.Deadline, Project.Name, TaskType.Name FROM Task JOIN TaskType ON Task.IdTaskType = TaskType.IdTaskType JOIN Project ON Task.IdProject = Project.IdProject WHERE IdAssignedTo = @IdAssignedTo ORDER BY DeadLine DESC", this._connection))
         {
             command.Parameters.AddWithValue("@IdAssignedTo", idTeamMember);
             await using SqlDataReader reader = await command.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                tasks.Add(new TaskInfo((int)reader[0], (string)reader[1], (string)reader[2], (DateTime)reader[3],
-                    (int)reader[4],
-                    (string)reader[5], (int)reader[6],
-                    (int)reader[7]));
+                tasks.Add(new TaskInfo((string)reader[0], (string)reader[1], (DateTime)reader[2],
+                    (string)reader[3],
+                    (string)reader[4]));
             }
         }
 
